ddc7e1300f3212b9990740ac91f296a952de4bb6
Ritik Jangid <ritikjangir.06@gmail.com>
2023-06-08 19:51:52 +0530

    added database models

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
+const User = require('./models/user');
+const Job = require('./models/job');
+
 dotenv.config();
 
 const app = express();
---
+++ models/job.js
@@ -0,0 +1,18 @@
+// models/recipe.js
+const mongoose = require('mongoose');
+
+const jobSchema = new mongoose.Schema({
+  companyName: {type: String, required: true},
+  position: {type: String, required: true},
+  monthlySalary: {type: Number, required: true},
+  jobType: {type: String, required: true},
+  internshipDuration: String,
+  workingMode: {type: String, required: true},
+  jobDescription: {type: String, required: true},
+  aboutCompany: {type: String, required: true},
+  skills: {type: String, required: true},
+  logo: String,
+  location: String,
+});
+
+module.exports = mongoose.model('Job', jobSchema);
