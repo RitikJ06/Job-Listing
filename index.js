5a5f5b955427afd1c2c6e570ff586447d5c5daa5
Ritik Jangid <ritikjangir.06@gmail.com>
2023-06-09 08:56:44 +0530

    added error handler middleware

--- index.js
+++ index.js
@@ -15,18 +15,22 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("./public"));
 
+
 app.get("/health", (req, res) => {
   res.send("Everything is working fine!");
 });
 
-app.post("/login", async (req, res) => {
+app.get('/users', (req, res) => {
+  User.find().then((users) => res.json(users)).catch((error) => next(error));
+})
+app.post("/login", async (req, res, next) => {
     const { email, password } = req.body;
     try {
         const user = await User.findOne({ email });
         if (user) {
             let passwordMatch = await bcrypt.compare(password, user.password);
             if (passwordMatch) {
-                const jwtToken = jwt.sign( { email }, process.env.JWT_SECRET_KEY,
+              const jwtToken = jwt.sign( { email }, process.env.JWT_SECRET_KEY,
                 { expiresIn: "2h" } );
                 return res.send({
                     status: "SUCCESS",
@@ -34,12 +38,12 @@ app.post("/login", async (req, res) => {
                     jwtToken,
                 });
             }
-        }
+          }
         res.send({ status: "FAIL", message: "Incorrect credentials" });
     } catch (error) {
-        res.send({ error });
+      next(new Error("Something went wrong! Please try after some time."));
     }
-});
+  });
 
 app.post("/register", async (req, res) => {
   const { name, email, mobile, password } = req.body;
@@ -59,19 +63,38 @@ app.post("/register", async (req, res) => {
         email,
         mobile,
         password: encryptedPassword,
-    });
+      });
     const jwtToken = jwt.sign({ email }, process.env.JWT_SECRET_KEY, {
-        expiresIn: "2h",
+      expiresIn: "2h",
     });
     res.send({
-        status: "SUCCESS",
+      status: "SUCCESS",
         message: "User created successfully",
         jwtToken,
-    });
+      });
   } catch (error) {
-        res.send({ error });
+    next(new Error("Something went wrong! Please try after some time."));
   }
 });
+
+
+app.use((req, res, next) => {
+  const err = new Error("Not found")
+  err.status = 404
+  next(err);
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message
+    }
+  })
+})
+
 
 app.listen(process.env.PORT, () => {
   mongoose
