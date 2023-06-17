6f7db28f4798845641a09c1190850cc35066ee2d
Ritik Jangid <ritikjangir.06@gmail.com>
2023-06-17 20:47:48 +0530

    header added

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,12 +1,64 @@
 import React from 'react'
 import './Home.css'
 import Header from './../common/header/Header'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+function Skill(props) {
+  return (
+    <div className='skillBlock'>
+      <span className='skillName'>
+        {props.currentSkill}
+      </span>
+      <span onClick={() => {
+        console.log( [...props.skills.splice(props.skills.indexOf(props.currentSkill), 1)])
+        props.setSkills( [...props.skills.splice(props.skills.indexOf(props.currentSkill), 1)] );
+      }} className='closeButton'>
+        X
+      </span>
+    </div>
+  )
+}
+
 
 export default function Home() {
+  const [allSkills, setAllSkills] = useState(["first", "second"]);
+  const [skills, setSkills] = useState(["CSS"]);
+  useEffect( () => {
+    let jobs = axios.get('http://localhost:8000/api/jobs')   
+    console.log(jobs)
+
+  }, [])
+  
+
   return (
     <main>
       <Header />
 
+      <div className='searchSection'>
+        <div className='searchBoxWrapper'>
+            <input className='searchInput' type="text" placeholder="&#xf002; Type any job title"></input>
+            <div className='skillsSelectorWrapper'> 
+              <div className='skillsSelector'>
+                <select id="skills" name="skills" disabled={!allSkills.length}
+                onChange={(item) => {
+                  if(!skills.includes(item.currentTarget.value)  && item.currentTarget.value!==""){
+                    setSkills([...skills, item.currentTarget.value])
+                  }
+                }}
+                >
+                  <option value="">Skills</option>
+                  {allSkills.map((item) => <option key={item} value={item}>{item}</option>
+                  )}
+                </select> 
+              </div>
+              <div className='skillsList'>
+                {skills.map((item) => <Skill key={item} currentSkill={item} skills={skills} setSkills={setSkills} /> ) }
+              </div>
+            </div>
+
+        </div>
+      </div>
     </main>
   )
 }
--- client/src/components/common/header/Header.jsx
+++ client/src/components/common/header/Header.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import userImg from './user-image.png'
 
-export default function Header() {
+export default function Header(props) {
+  const navigate = useNavigate();
+  const [name, setName] = useState();
+  const [data, setData] = useState({jwtToken: false});
+
+  
+  useEffect(() => {
+    let userData = JSON.parse(localStorage.getItem('data'));
+    if(!userData){
+      userData = {jwtToken : false};
+    }
+    setData(userData);
+    setName(userData.name);
+  }, [])
+
   return (
-    <div>Header</div>
+    <div className='headerBlock'>
+      <h2>Jobfinder</h2>
+      <div className='navItemsWrapper'>
+        { !data.jwtToken ? 
+            <>
+            <button onClick={() => {navigate('./login')}} className='loginButton' >Login</button>
+            <button onClick={() => {navigate('./register')}} className='registerButton'>Register</button>
+            </>
+          :
+            <>
+              <span className='logoutButton' onClick={() => {localStorage.removeItem('data');  navigate('/')}}>Logout</span>
+              <span className='userName'>Hello {name}</span>
+              <img className='userImage' src={userImg} alt='use icon' />
+            </>
+        }
+      </div>
+    </div>
   )
 }
